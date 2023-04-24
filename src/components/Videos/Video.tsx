c4bcad30c250704235ebf992b8894984541ce070
Tanjila Akter Shamima <tanjila.akter@bjitgroup.com>
2023-04-24 12:21:37 +0600

    update

--- src/components/Videos/Video.tsx
+++ src/components/Videos/Video.tsx
@@ -9,7 +9,7 @@ const Video = ({ video }: Props) => {
     <div className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
       <div className="w-full flex flex-col">
         <div className="relative">
-          <Link to="/videos/1">
+          <Link to={`/videos/${video.id}`}>
             <img
               src={video.thumbnail}
               className="w-full h-auto"
@@ -30,7 +30,7 @@ const Video = ({ video }: Props) => {
           />
 
           <div className="flex flex-col">
-            <Link to="/videos/1">
+            <Link to={`/videos/${video.id}`}>
               <p className="text-slate-900 text-sm font-semibold">
                 {video.title}
               </p>
--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -7,17 +7,19 @@ import { useGetSingleVideoQuery } from "../features/API/apiSlice";
 const VideoPage = () => {
   const {videoId} = useParams()
   const {data: video} = useGetSingleVideoQuery(videoId || "")
+  console.log("video", video)
+  console.log("videoId", videoId)
   return (
     <section className="pt-6 pb-20 min-h-[calc(100vh_-_157px)]">
       <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
-        <div className="grid grid-cols-3 gap-2 lg:gap-8">
+        {video?.id && <div className="grid grid-cols-3 gap-2 lg:gap-8">
           <div className="col-span-full w-full space-y-8 lg:col-span-2">
             <Player videoLink={video.link} title={video.title} />
             <VideoDescription video={video} />
           </div>
 
           <RelatedVideoList />
-        </div>
+        </div>}
       </div>
     </section>
   );
